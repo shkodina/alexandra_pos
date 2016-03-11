8bf9619505319f71297b9baf40614ceb5d11a97f
shkodina <blobby@yandex.ru>
2016-03-12 01:34:56 +0300

    исправил ошибку, что второе письмо не отсылалось. теперь все пучком. можно продумывать алгоритм выбора времени создания бэкапа

--- mymodules/backuper.js
+++ mymodules/backuper.js
@@ -6,24 +6,28 @@ var backuper = {
     init : function (){
         backuper.fs = require('fs');
         backuper.archiver = require('archiver');
-        backuper.archive = backuper.archiver('zip');
+
 
     }
     , createBackup : function(confdata){
-        var output = backuper.fs.createWriteStream(confdata.resultname);
 
-        output.on('close', function() {
-            console.log(backuper.archive.pointer() + ' total bytes');
-            console.log('archiver has been finalized and the output file descriptor has closed.');
-        });
+        var archive = backuper.archiver('zip');
 
-        backuper.archive.on('error', function(err) {
+        archive.on('error', function(err) {
             throw err;
         });
 
-        backuper.archive.pipe(output);
+        var output = backuper.fs.createWriteStream(confdata.resultname);
 
-        backuper.archive
+        output.on('close', function() {
+            console.log(archive.pointer() + ' total bytes');
+            console.log('archiver has been finalized and the output file descriptor has closed.');
+        });
+
+
+        archive.pipe(output);
+
+        archive
             .append(backuper.fs.createReadStream(confdata.filepath), { name: confdata.filename })
            // .append(backuper.fs.createReadStream(file2), { name: 'checks.db' })
             .finalize();
